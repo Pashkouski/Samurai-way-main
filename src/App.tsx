7fd9662bc16b8776ea42a57f115c8d5cc4d937f2
pashkouski <satiym@gmail.com>
2022-11-24 11:20:40 +0300

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='appWrapper'>
+            <header className="header">
+                <img
+                    src="https://cdn.dribbble.com/userupload/3158902/file/original-7c71bfa677e61dea61bc2acd59158d32.jpg?resize=400x0"
+                    alt="img"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fw800/png-vector/20200530/ourmid/pngtree-great-wall-scenery-png-image_2214406.jpg"
+                        alt=""/>
+                </div>
+                <div>
+                    ava + d
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                    <div>
+                        post 3
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
