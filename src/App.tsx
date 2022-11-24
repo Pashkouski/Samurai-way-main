69a7a3ae4a379a07d3697583521ca541389da0e8
pashkouski <satiym@gmail.com>
2022-11-24 10:33:08 +0300

    07

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-        <div> one div </div>
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+
+const Technologies = () => {
+    return (
         <div>
             <ul>
                 <li>css</li>
@@ -13,8 +21,17 @@ function App() {
                 <li>react</li>
             </ul>
         </div>
-    </div>
-  );
+    )
 }
+
+const Header = () => {
+    return (<div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
 
 export default App;
